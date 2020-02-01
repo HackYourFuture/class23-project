9d4fd8e8a46b9bc1883086e251d3a0f0d683a8bf
es <esrazengn00@gmail.com>
2020-02-01 16:17:13 +0100

    Update view.js to get top 10 products

--- pages/api/view.js
+++ pages/api/view.js
@@ -1,14 +1,14 @@
-import Product from "../../models/Product";
-import connectDb from "../../utils/connectDb";
+import Product from '../../models/Product';
+import connectDb from '../../utils/connectDb';
 
 connectDb();
 
 export default async (req, res) => {
   switch (req.method) {
-    case "POST":
+    case 'POST':
       await handlePostRequest(req, res);
       break;
-    case "GET":
+    case 'GET':
       await handleGetRequest(req, res);
       break;
     default:
@@ -20,14 +20,10 @@ export default async (req, res) => {
 async function handlePostRequest(req, res) {
   const { productId } = req.body;
   try {
-    await Product.findOneAndUpdate(
-      { _id: productId },
-      { $inc: { numberOfViews: 1 } }
-    );
-    res.status(200).send("incremented product view");
+    await Product.findOneAndUpdate({ _id: productId }, { $inc: { numberOfViews: 1 } });
+    res.status(200).send('incremented product view');
   } catch (error) {
-    console.log(error);
-    res.status(500).send("Error incrementing number of product views");
+    res.status(500).send('Error incrementing number of product views');
   }
 }
 
@@ -36,31 +32,37 @@ async function handleGetRequest(req, res) {
     return res.status(401).send('No authorization token');
   }
   try {
+    const topViewedTenProducts = await Product.aggregate([
+      {
+        $sort: { numberOfViews: -1 },
+      },
+      {
+        $limit: 10,
+      },
+    ]);
     // group products by their categories and create an array of products for each category.
     const groupedProducts = await Product.aggregate([
       {
         $group: {
-          _id: "$category",
-          products: { $push: "$$ROOT" }
-        }
-      }
+          _id: '$category',
+          products: { $push: '$$ROOT' },
+        },
+      },
     ]);
-    // console.log("groupped", groupedProducts[0].products);
 
-    const results = groupedProducts.map(group => {
-      const sortedProducts = group.products.sort(
-        (a, b) => b.numberOfViews - a.numberOfViews
-      );
+    const topProductsOfCategory = groupedProducts.map(group => {
+      const sortedProducts = group.products.sort((a, b) => b.numberOfViews - a.numberOfViews);
       const products = sortedProducts.slice(0, 3);
       return {
         _id: group._id,
-        products
+        products,
       };
     });
 
+    const results = { topViewedTenProducts, topProductsOfCategory };
+
     res.status(200).json(results);
   } catch (error) {
-    console.error(error);
-    res.status(500).json(error);
+    res.status(500).send('Error getting number of product views');
   }
 }
