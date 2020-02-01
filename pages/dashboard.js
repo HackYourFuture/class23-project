7e743414a005842741d4e055063bc2b6d0da03a1
es <esrazengn00@gmail.com>
2020-02-01 16:19:01 +0100

    Update dashboard to add tab component and additional styling

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,91 +1,90 @@
-import Chart from "../components/Dashboard/Chart";
-import baseUrl from "../utils/baseUrl";
-import axios from "axios";
-import { parseCookies } from "nookies";
-import { Doughnut } from "react-chartjs-2";
-import { Grid, GridColumn } from "semantic-ui-react";
+import { Grid, Tab } from 'semantic-ui-react';
+import { Doughnut } from 'react-chartjs-2';
+import { parseCookies } from 'nookies';
+import axios from 'axios';
+import uuid from 'uuid';
+import baseUrl from '../utils/baseUrl';
+import Chart from '../components/Dashboard/Chart';
 
-function Dashboard({ topViewedProducts }) {
-  console.log(topViewedProducts);
+function Dashboard({ topViewedTenProducts, topProductsOfCategory }) {
   const data = {
-    labels: topViewedProducts.map(
-      category => `${category.products[0].name} (${category._id})`
-    ),
+    labels: topViewedTenProducts.map(product => product.name),
     datasets: [
       {
-        label: "# of Views",
-        data: topViewedProducts.map(
-          category => category.products[0].numberOfViews
-        ),
+        label: '# of Views',
+        data: topViewedTenProducts.map(product => product.numberOfViews),
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)"
+          '#8dd3c7',
+          '#de425b',
+          '#bebada',
+          '#fb8072',
+          '#80b1d3',
+          '#fdb462',
+          '#ffa600',
+          '#f07c5c',
+          '#488f31',
         ],
-
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)"
-        ],
-        borderWidth: 1
-      }
-    ]
+        borderWidth: 1,
+      },
+    ],
   };
 
-  return (
-    <>
-      <h2>Most Viewed Chart</h2>
-      <Chart data={data} />
-      <Grid container stackable columns={2} style={{ margintop: "40px" }}>
-        {topViewedProducts.map(data => {
-          const dataDonut = {
-            labels: data.products.map(product => {
-              return `${product.name}`;
-            }),
-            datasets: [
-              {
-                data: data.products.map(product => product.numberOfViews),
-                backgroundColor: ["#ff677d", "#ffae8f", "#6f5a7e"],
-                borderColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)"
-                ],
-                borderWidth: 1
-              }
-            ]
-          };
+  function getDoughnutData(category) {
+    return {
+      labels: category.products.map(product => product.name),
+      datasets: [
+        {
+          data: category.products.map(product => product.numberOfViews),
+          backgroundColor: ['#8dd3c7', '#fdc086', '#bebada'],
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
 
-          return (
-            <GridColumn textAlign="center">
-              <div className="chart">
-                <h2>{`${data._id} chart`}</h2>
-                <Doughnut
-                  data={dataDonut}
-                  width={100}
-                  height={50}
-                  margin={{ top: 10, bottom: 28, left: 50, right: 10 }}
-                />
-              </div>
-            </GridColumn>
-          );
-        })}
-      </Grid>
-    </>
-  );
+  const panes = [
+    {
+      menuItem: 'Top viewed products',
+      render: () => (
+        <Tab.Pane>
+          <Chart data={data} />
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: 'Top three products of each category',
+      render: () => (
+        <Tab.Pane>
+          <Grid container stackable columns={2} style={{ marginTop: '40px' }}>
+            {topProductsOfCategory.map(category => {
+              return (
+                <Grid.Column textAlign="center" key={uuid()}>
+                  <h2>{category._id.charAt(0).toUpperCase() + category._id.slice(1)}</h2>
+                  <Doughnut
+                    data={getDoughnutData(category)}
+                    width={100}
+                    height={70}
+                    margin={{ top: 10, bottom: 28 }}
+                  />
+                </Grid.Column>
+              );
+            })}
+          </Grid>
+        </Tab.Pane>
+      ),
+    },
+  ];
+
+  return <Tab menu={{ color: 'teal', inverted: true }} panes={panes} />;
 }
+
 Dashboard.getInitialProps = async ctx => {
   const { token } = parseCookies(ctx);
   const payload = { headers: { Authorization: token } };
   const url = `${baseUrl}/api/view`;
   const response = await axios.get(url, payload);
-  return { topViewedProducts: response.data };
+  const { topViewedTenProducts, topProductsOfCategory } = response.data;
+  return { topViewedTenProducts, topProductsOfCategory };
 };
+
 export default Dashboard;
