de28500832c9e6055918f3ff427da6055e9f7776
elicarcar <elisabetta.carcar@gmail.com>
2020-02-01 14:43:20 +0100

    changed UA for shared usage

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 export const initGA = () => {
   console.log("GA init");
-  ReactGA.initialize("UA-114127926-3");
+  ReactGA.initialize("UA-157526897-1");
 };
 export const logPageView = () => {
   console.log(
