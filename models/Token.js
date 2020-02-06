b5fadb814eeef57bdc459bcb6ef06bd28f67d361
es <esrazengn00@gmail.com>
2020-02-06 13:49:58 +0100

    Updated token model added expiry index

--- models/Token.js
+++ models/Token.js
@@ -2,22 +2,24 @@ import mongoose from 'mongoose';
 
 const { String, ObjectId } = mongoose.Schema.Types;
 
-const TokenSchema = new mongoose.Schema({
-  user: {
-    type: ObjectId,
-    required: true,
-    ref: 'User',
+const TokenSchema = new mongoose.Schema(
+  {
+    user: {
+      type: ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    token: {
+      type: String,
+      required: true,
+    },
   },
-  token: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-    expires: 604800,
-  },
-});
+);
+
+//with index, the token will be expired after 12 hours and deleted from db
+TokenSchema.index({ createdAt: 1 }, { expireAfterSeconds: 43200 });
 
 export default mongoose.models.Token || mongoose.model('Token', TokenSchema);
