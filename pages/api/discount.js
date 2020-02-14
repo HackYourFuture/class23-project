a59a88067ddcb13b099acbd6c6558fad892c523c
msnmz <snmez.muharrem@gmail.com>
2020-02-14 06:59:40 +0100

    Adds partially update: not stable for now

--- pages/api/discount.js
+++ pages/api/discount.js
@@ -1,14 +1,15 @@
 import Product from '../../models/Product';
 import Discount from '../../models/Discount';
+import Cart from '../../models/Cart';
 import User from '../../models/User';
 import connectDb from '../../utils/connectDb';
 import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
 import {
   checkDiscountForRequiredProps,
   checkDiscountIsOK,
   getRequiredPropsListForDiscount,
-  UNIT_TYPES
+  UNIT_TYPES,
+  DISCOUNT_TYPES
 } from '../../utils/discount';
 
 connectDb();
@@ -84,6 +85,7 @@ async function handlePostRequest(req, res) {
   try {
     // Get the required fields & check if they exist
     checkDiscountForRequiredProps(req.body); // if not OK, will throw error
+    if (!checkDiscountIsOK(req.body)) throw new Error('Missing arguments for discount.');
     // Verify the token
     const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
     // Find the user
@@ -135,6 +137,84 @@ async function handlePostRequest(req, res) {
             console.log(`Multiple products updated according to a category with response: ${resp}`);
             return res.status(200).send('Discount is created and products are updated successfully!');
           }
+        }
+      } else {
+        return res.status(401).send(`You don't have the permission to create a discount!`);
+      }
+    } else {
+      res.status(404).send('User not found');
+    }
+  } catch (error) {
+    res.status(403).send(error.message);
+  }
+}
+
+// Activates or Deactivates a discount
+// And deals with the cascade update operation towards products in the cards
+async function handlePutRequest(req, res) {
+  // Check if the user is authorized
+  if (!('authorization' in req.headers)) {
+    return res.status(401).send('No authorization token');
+  }
+  // Get the required fields & check if they exist
+  const { isActive, discountId } = req.body;
+  if (!discountId && isActive === undefined && typeof isActive !== 'boolean') {
+    return res.status(403).send('Missing or bad argument: isActive, discountId');
+  }
+  try {
+    // Verify the token
+    const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    // Find the user
+    const user = await User.findOne({ _id: userId });
+    // If the user exists
+    if (user) {
+      // Check if the user is authorized to update discount
+      if (user.role === 'admin' || user.role === 'root') {
+        // Find & update discount
+        const discount = await Discount.findOne({ _id: discountId });
+        if (isActive) {
+          // Check the dates & decide if the operation is possible
+          const now = new Date();
+          if (now < discount.startDate) {
+            return res.status(403).send('Discount could not be set as active before start date. Try again later.');
+          } else if (now > discount.endDate) {
+            return res.status(403).send('Discount could not be set as active after it is expired.');
+          }
+        }
+        await Discount.findOneAndUpdate({ _id: discountId }, { isActive });
+        // Update products about discount - cascade update for products in the cards
+        // Get all the product ids related to this discount
+        const products = await Product.find({ 'discounts._id': discountId }).distinct('_id');
+        // Get all the cards with these products
+        if (isActive) {
+          // Find the products which will be discounted more with these discount and apply the discount
+          let findQuery;
+          if (discount.discountType === DISCOUNT_TYPES.amountBased) {
+            if (discount.multipleUnits) {
+              if (discount.unitType === UNIT_TYPES.product) {
+                findQuery = [
+                  { products: { $all: discount.products } },
+                ]
+              } else {
+
+              }
+            } else {
+
+            }
+          } else {// relation based
+
+          }
+          await Cart.update(
+            {
+              $and: [
+                { 'products.product._id': { $in: products } },
+                { 'products.discountAmount': { $lt: { $multiply: ['$products.product.price', discount.discountPercentage / 100] } } },
+                ...findQuery
+              ]
+            }
+          );
+
+        } else {
         }
       } else {
         return res.status(401).send(`You don't have the permission to create a discount!`);
