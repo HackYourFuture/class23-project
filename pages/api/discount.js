cdfe7b96a358be4b84e9702389edbc10599bd179
msnmz <snmez.muharrem@gmail.com>
2020-02-15 14:35:09 +0100

    Fixes bug: Discount delete handler query params

--- pages/api/discount.js
+++ pages/api/discount.js
@@ -433,7 +433,7 @@ async function handleDeleteRequest(req, res) {
     return res.status(401).send("No authorization token");
   }
   // Get the required field & check if it exists
-  const { discountId } = req.body;
+  const { discountId } = req.query;
   if (!discountId) {
     return res
       .status(403)
