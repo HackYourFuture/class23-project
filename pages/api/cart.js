608effec14a07447fe5255d3809824d4318190a2
msnmz <snmez.muharrem@gmail.com>
2020-02-16 01:01:15 +0100

    Adjusts product removel from the cart

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -64,7 +64,7 @@ async function handlePutRequest(req, res) {
       ObjectId(productId).equals(doc.product)
     );
 
-    const discountInfo = await isDiscountApplicableForCart(cart, productId, productExists);
+    const discountInfo = await isProductsDiscountApplicableForCart(cart, productId, productExists);
 
     // If product exists, increment quantity (by number provided to request)
     if (productExists) {
@@ -81,27 +81,10 @@ async function handlePutRequest(req, res) {
         { $addToSet: { products: newProduct } }
       );
     }
-    let findQuery = {};
+
     const discount = discountInfo.product.discount;
     if (discountInfo.isSuitable) {
-      if (discount.unitType === UNIT_TYPES.product) {
-        if (discount.multipleUnits) { // products
-          findQuery = { 'products.product': { $in: discount.products.map(p => p._id) } };
-        } else { // product
-          findQuery = { 'products.product': productId };
-        }
-      } else { // unit category
-        if (discount.multipleUnits) { // categories
-          findQuery = { 'products.product.category': { $in: discount.categories } };
-        } else { // category
-          findQuery = { 'products.product.category': discount.category };
-        }
-      }
-      await Cart.update(
-        { _id: cart._id, ...findQuery },
-        { $set: { 'products.$.discount': discount } },
-        { multi: true }
-      );
+      await addDiscountToCartInDeactivatedMode(discount, cart);
     }
     if (discountInfo.isApplicable) {
       await activateDiscountForCart(discount);
@@ -123,14 +106,83 @@ async function handleDeleteRequest(req, res) {
       req.headers.authorization,
       process.env.JWT_SECRET
     );
-    const cart = await Cart.findOneAndUpdate(
-      { user: userId },
-      { $pull: { products: { product: productId } } },
-      { new: true }
-    ).populate({
+    // get the cart
+    const oldCart = await Cart.findOne({ user: userId }).populate({
       path: "products.product",
       model: "Product"
+    }).populate({
+      path: "products.discount",
+      model: "Discount"
+    }).populate({
+      path: "products.discount.products",
+      model: "Product"
+    }).populate({
+      path: "products.discount.product",
+      model: "Product"
     });
+
+    // get the product element from cart
+    const cartProduct = oldCart.products.find(doc => ObjectId(productId).equals(doc.product));
+    let cart = {};
+    // check if a discount set to the product
+    if (cartProduct.discount) {
+      // Remove the discount from the cart
+      await Cart.update(
+        { user: userId },
+        { $unset: { 'products.$[element].discount': 1 } },
+        {
+          multi: true,
+          arrayFilters: [{ 'element.discount': cartProduct.discount._id }]
+        }
+      );
+      // remove the product from the cart
+      cart = await Cart.findOneAndUpdate(
+        { user: userId },
+        { $pull: { products: { product: productId } } },
+        { new: true }
+      ).populate({
+        path: "products.product",
+        model: "Product"
+      }).populate({
+        path: "products.discount",
+        model: "Discount"
+      }).populate({
+        path: "products.discount.products",
+        model: "Product"
+      }).populate({
+        path: "products.discount.product",
+        model: "Product"
+      });
+
+      // Check the applicability of the discount for the cart
+      const discountInfo = isDiscountApplicableForCartWithRemovedProduct(cart, cartProduct.discount);
+      if (discountInfo.isSuitable) {
+        cart = await addDiscountToCartInDeactivatedMode(discount, cart);
+      }
+      if (discountInfo.isApplicable) {
+        cart = await activateDiscountForCart(discount);
+      }
+    } else {
+      // remove the product from the cart
+      cart = await Cart.findOneAndUpdate(
+        { user: userId },
+        { $pull: { products: { product: productId } } },
+        { new: true }
+      ).populate({
+        path: "products.product",
+        model: "Product"
+      }).populate({
+        path: "products.discount",
+        model: "Discount"
+      }).populate({
+        path: "products.discount.products",
+        model: "Product"
+      }).populate({
+        path: "products.discount.product",
+        model: "Product"
+      });
+    }
+
     res.status(200).json(cart.products);
   } catch (error) {
     console.error(error);
@@ -139,7 +191,7 @@ async function handleDeleteRequest(req, res) {
 }
 
 
-async function isDiscountApplicableForCart(cart, productId, productExists) {
+async function isProductsDiscountApplicableForCart(cart, productId, productExists) {
   // Check for discount availability
   let isDiscountOnline = false,
     isCartProvidesRequirementsForDiscount = true,
@@ -201,6 +253,84 @@ async function isDiscountApplicableForCart(cart, productId, productExists) {
   return { product, isApplicable: discount.isActive, isSuitable: true };
 }
 
+async function isDiscountApplicableForCartWithRemovedProduct(cart, discount) {
+  // Check for discount availability
+  let isDiscountOnline = false,
+    isCartProvidesRequirementsForDiscount = true,
+    isDiscountAppliedBefore = false;
+  // Check if the discount is not expired
+  isDiscountOnline = !isDiscountExpired(discount) && isDiscountStarted(discount);
+  if (!isDiscountOnline) {
+    return { isApplicable: false, isSuitable: false };
+  }
+  // Check if the card has everything for discount
+  if (discount.unitType === UNIT_TYPES.product) { // product
+    if (discount.multipleUnits) { // check for the products
+      discount.products.forEach(prod => {
+        if (!cart.products.some(doc => ObjectId(prod._id).equals(doc.product))) {
+          isCartProvidesRequirementsForDiscount = false;
+        }
+      });
+    } else { // check for the product amount
+      isCartProvidesRequirementsForDiscount = false;
+    }
+  } else { // category
+    if (discount.multipleUnits) { // check for the categories
+      discount.categories.forEach(cat => {
+        if (!cart.products.some(doc => doc.product.category === cat)) {
+          isCartProvidesRequirementsForDiscount = false;
+        }
+      });
+    } else { // check for the category's products amount
+      let total = cart.products.reduce((total, doc) => {
+        if (doc.product.category === discount.category) return total + 1;
+        else return total;
+      }, 0);
+      if (total < discount.amountRequired) {
+        isCartProvidesRequirementsForDiscount = false;
+      }
+    }
+  }
+  if (!isCartProvidesRequirementsForDiscount) {
+    return { isApplicable: false, isSuitable: false };
+  }
+  return { isApplicable: discount.isActive, isSuitable: true };
+}
+
+async function addDiscountToCartInDeactivatedMode(discount, cart) {
+  let findQuery = {};
+  if (discount.unitType === UNIT_TYPES.product) {
+    if (discount.multipleUnits) { // products
+      findQuery = { 'products.product': { $in: discount.products.map(p => p._id) } };
+    } else { // product
+      findQuery = { 'products.product': discount.product._id };
+    }
+  } else { // unit category
+    if (discount.multipleUnits) { // categories
+      findQuery = { 'products.product.category': { $in: discount.categories } };
+    } else { // category
+      findQuery = { 'products.product.category': discount.category };
+    }
+  }
+  return await Cart.update(
+    { _id: cart._id, ...findQuery },
+    { $set: { 'products.$.discount': discount } },
+    { multi: true }
+  ).populate({
+    path: "products.product",
+    model: "Product"
+  }).populate({
+    path: "products.discount",
+    model: "Discount"
+  }).populate({
+    path: "products.discount.products",
+    model: "Product"
+  }).populate({
+    path: "products.discount.product",
+    model: "Product"
+  });
+}
+
 async function activateDiscountForCart(discount) {
   // Find the cart with products which is discounted with this discount
   const anItemDiscountAmount = {
@@ -214,7 +344,7 @@ async function activateDiscountForCart(discount) {
       100
     ]
   };
-  await Cart.update(
+  return await Cart.update(
     { "products.discount._id": discount._id },
     {
       $set: {
@@ -237,5 +367,17 @@ async function activateDiscountForCart(discount) {
       multi: true,
       arrayFilters: [{ "element.discount._id": discount._id }]
     }
-  );
+  ).populate({
+    path: "products.product",
+    model: "Product"
+  }).populate({
+    path: "products.discount",
+    model: "Discount"
+  }).populate({
+    path: "products.discount.products",
+    model: "Product"
+  }).populate({
+    path: "products.discount.product",
+    model: "Product"
+  });
 }
