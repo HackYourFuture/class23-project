8e69a5bb27111e04282a4a42bcea7984a427f94e
mehmetekinci <mehmetekinci1989@gmail.com>
2020-02-16 13:22:55 +0100

    added 'purchase information mail feature'

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { logEvent } from "../utils/analytics";
-import { useRouter } from "next/router";
-import { Segment, Modal, Image, Button, Header } from "semantic-ui-react";
-import CartItemList from "../components/Cart/CartItemList";
-import CartSummary from "../components/Cart/CartSummary";
-import { parseCookies } from "nookies";
-import axios from "axios";
-import baseUrl from "../utils/baseUrl";
-import cookie from "js-cookie";
-import catchErrors from "../utils/catchErrors";
+import { useState } from 'react';
+import { logEvent } from '../utils/analytics';
+import { useRouter } from 'next/router';
+import { Segment, Modal, Image, Button, Header } from 'semantic-ui-react';
+import CartItemList from '../components/Cart/CartItemList';
+import CartSummary from '../components/Cart/CartSummary';
+import { parseCookies } from 'nookies';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
+import cookie from 'js-cookie';
+import catchErrors from '../utils/catchErrors';
 
 function Cart({ products, user }) {
   const router = useRouter();
@@ -16,35 +16,56 @@ function Cart({ products, user }) {
   const [success, setSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(true);
+  const [lastOrder, setLastOrder] = React.useState({});
 
   async function handleRemoveFromCart(productId) {
     const url = `${baseUrl}/api/cart`;
-    const token = cookie.get("token");
+    const token = cookie.get('token');
     const payload = {
       params: { productId },
-      headers: { Authorization: token }
+      headers: { Authorization: token },
     };
     const response = await axios.delete(url, payload);
     setCartProducts(response.data);
-    logEvent("User", `User ${user.name} removed product from their cart`);
+    logEvent('User', `User ${user.name} removed product from their cart`);
   }
 
   async function handleCheckout(paymentData) {
     try {
       setLoading(true);
       const url = `${baseUrl}/api/checkout`;
-      const token = cookie.get("token");
+      const token = cookie.get('token');
       const payload = { paymentData };
       const headers = { headers: { Authorization: token } };
-      await axios.post(url, payload, headers);
+      const response = await axios.post(url, payload, headers);
+      setLastOrder(response.data);
       setSuccess(true);
-      logEvent("User", `User ${user.name} made a payment! `);
+      logEvent('User', `User ${user.name} made a payment! `);
     } catch (error) {
       catchErrors(error, window.alert);
     } finally {
       setLoading(false);
     }
   }
+
+  React.useEffect(() => {
+    if (success) {
+      handleInfoMail();
+      return;
+    }
+
+    async function handleInfoMail() {
+      try {
+        const url = `${baseUrl}/api/info-mail`;
+        const token = cookie.get('token');
+        const payload = { lastOrder, email: user.email };
+        const headers = { headers: { Authorization: token } };
+        await axios.put(url, payload, headers);
+      } catch (error) {
+        catchErrors(error, window.alert);
+      }
+    }
+  }, [success]);
 
   return (
     <>
@@ -68,9 +89,9 @@ function Cart({ products, user }) {
           open={isOpen}
           size="large"
           style={{
-            marginTop: "0px !important",
-            position: "relative",
-            top: "20px"
+            marginTop: '0px !important',
+            position: 'relative',
+            top: '20px',
           }}
         >
           <Header content="You can rate your products from your order list now!" />
@@ -78,7 +99,7 @@ function Cart({ products, user }) {
             <Image floated="left" src="../static/click-to-rate.png" />
           </Modal.Content>
           <Modal.Actions>
-            <Button color="green" onClick={() => router.push("/account")}>
+            <Button color="green" onClick={() => router.push('/account')}>
               I'd love to!
             </Button>
             <Button color="red" onClick={() => setIsOpen(false)}>
