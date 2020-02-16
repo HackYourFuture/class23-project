24e1e190c5c7b8a3952a157f0319b7aeb68aca64
msnmz <snmez.muharrem@gmail.com>
2020-02-16 11:43:36 +0100

    Fixes bug: adjusts subtotal

--- utils/calculateCartTotal.js
+++ utils/calculateCartTotal.js
@@ -1,6 +1,9 @@
 function calculateCartTotal(products) {
   const total = products.reduce((acc, el) => {
     acc += el.product.price * el.quantity;
+    if (el.discount && el.discountApplied) {
+      acc -= el.discountAmount;
+    }
     return acc;
   }, 0);
   const cartTotal = ((total * 100) / 100).toFixed(2);
