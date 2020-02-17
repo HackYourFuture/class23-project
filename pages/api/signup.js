15d7ca596e2da2f26f8d65ef330b2ddee87dda56
msnmz <snmez.muharrem@gmail.com>
2020-02-17 18:03:20 +0100

    Add validations for password from the branch email-verification

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -7,18 +7,57 @@ import Token from '../../models/Token';
 import connectDb from '../../utils/connectDb';
 import isEmail from 'validator/lib/isEmail';
 import isLength from 'validator/lib/isLength';
+import passwordValidator from 'password-validator';
+import equals from 'validator/lib/equals';
 
 connectDb();
 
 export default async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password, rePassword } = req.body;
   try {
     if (!isLength(name, { min: 3, max: 10 })) {
       return res.status(422).send('Name must be 3-10 characters long');
-    } else if (!isLength(password, { min: 6 })) {
-      return res.status(422).send('Password must be at least 6 characters');
+    } else if (!equals(password, rePassword)) {
+      return res.status(422).send('Passwords do not match');
     } else if (!isEmail(email)) {
       return res.status(422).send('Email must be valid');
+    }
+
+    const pwSchema = new passwordValidator();
+    pwSchema
+      .is()
+      .min(8)
+      .has()
+      .uppercase()
+      .has()
+      .lowercase()
+      .has()
+      .digits()
+      .has()
+      .symbols()
+      .has()
+      .not()
+      .spaces();
+
+    const pwErrors = {
+      min: 'at least 8 characters',
+      uppercase: 'at least 1 uppercase',
+      lowercase: 'at least 1 lowercase',
+      digits: 'at least 1 digit',
+      symbols: 'at least 1 symbol',
+      spaces: 'no spaces',
+    };
+
+    const errorList = pwSchema.validate(password, { list: true });
+
+    if (errorList.length) {
+      const errorMessages = Object.assign(
+        {},
+        ...Object.entries(pwErrors).map(
+          ([key, prop]) => errorList.includes(key) && { [key]: prop },
+        ),
+      );
+      return res.status(422).send(Object.values(errorMessages));
     }
     const user = await User.findOne({ email });
 
