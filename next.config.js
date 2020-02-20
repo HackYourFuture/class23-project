004fb048b14fccf102d4199b2ad0954af4b3feb3
msnmz <snmez.muharrem@gmail.com>
2020-02-20 23:29:31 +0100

    add config to git

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+  env: {
+    MONGO_SRV: "mongodb+srv://root-dbuser:7b6Deek3BMzX2gk@hackyourshop-iysqr.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "hfjdkshfjkdhskfjghkfjldksaldkL4FJKLA365DJFLKDJNFU95UT894JGGWHJSB21IUhjkdh7eknl",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dy7xzflbz/image/upload",
+    CLOUDINARY_UPLOAD_PRESET: "tcrsfuau",
+    CLOUDINARY_CLOUD_NAME: "dy7xzflbz",
+    STRIPE_SECRET_KEY: "sk_test_dPiAzZ4STF4DIhGHO495RW6v00UMFwJjjk",
+    SENDGRID_USERNAME: 'apikey',
+    SENDGRID_PASSWORD: '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',
+  }
+};
