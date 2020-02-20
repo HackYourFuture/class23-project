c4ecd1f7d1feb760422efe1afc5cd99a9697a07e
msnmz <snmez.muharrem@gmail.com>
2020-02-20 23:25:17 +0100

    Updates base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://rabie-reactreserve.now.sh"
+    ? "https://hackyourshop.herokuapp.com/"
     : "http://localhost:3000";
 
 export default baseUrl;
