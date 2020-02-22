dce135079adb7cfbf40062f0ab6257c5e6620880
msnmz <snmez.muharrem@gmail.com>
2020-02-22 13:05:19 +0100

    Fixes base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://hackyourshopfinal.herokuapp.com"
+    ? "https://hackyourshop.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
